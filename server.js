e3baa764063cada07ba1a443307175eb7bdd86ff
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-08-28 19:50:11 -0600

    cors

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ import cors from "cors";
 
 const app = express(); //codigo, inicia a configurar
 const PORT = 3000; //es una configuracion
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 app.use(express.json());
 
 //endpoint
