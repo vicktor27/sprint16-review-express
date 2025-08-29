755372bf80f9b73875bfe508a7371b2740678412
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-08-28 19:45:14 -0600

    initial-commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+const app = express(); //codigo, inicia a configurar
+const PORT = 3000; //es una configuracion
+
+app.use(express.json());
+
+//endpoint
+app.get("/saludo", (req, res) => {
+  res.json({
+    message:
+      "Hola Arlente, esto vino desde Gomez Palacio Durango, hasta Queretaro",
+  });
+});
+
+//inicia el servidor
+app.listen(PORT, () => {
+  console.log("Servidor Ejecutandose en http://localhost:" + PORT);
+});
